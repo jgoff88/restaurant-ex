1040689eea8fba333ac396a982845a0936f2fb94
James Goff <james_goff88@outlook.com>
2022-01-04 23:39:35 -0600

    Add tailwind directives to global CSS

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Header from "./Header";
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+      </Head>
+      
+      <div >{children}</div>
+    </div>
+  );
+}
+Layout.defaultProps = {
+  title: "Restaruant | Great Food",
+  description: "A local restraunt with great food",
+  keywords: "food, seafood, bbq, great food, service",
+};
