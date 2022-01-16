08442659113040703463ca29aea5cdd260a49248
James Goff <james_goff88@outlook.com>
2022-01-15 22:49:58 -0600

    recent navbar changes

--- components/Header.js
+++ components/Header.js
@@ -2,34 +2,41 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className=' text-gray-600  border-b-4 '>
-      <nav className='container mx-auto my-auto flex flex-row items-center justify-between p-3 '>
+    <header className=' drop-shadow-md bg-white text-gray-600 text-lg'>
+      <nav className='container mx-auto my-auto flex flex-row items-center justify-between p-3 text-secondary '>
         <div className='place-content-center'>
           {" "}
           <Link href='/'>
-            <a href='/' className='content-center underline decoration-sky-500'>
-              Restaraunt
+            <a
+              href='/'
+              className='content-center no-underline hover:underline shadow-md'
+            >
+              <h3>Restaraunt</h3>
             </a>
           </Link>
         </div>
         <div className=''>
-          <ul className='flex  flex-row space-x-4 justify-between justify-items-start '>
+          <ul className='flex flex-row space-x-4 justify-between justify-items-start '>
             <li className='justify-around'>
               {" "}
               <Link href='/about'>
-                <a className='underline decoration-sky-500 '>About</a>
+                <a className='no-underline hover:underline  '>
+                  <h3>About</h3>
+                </a>
               </Link>
             </li>
             <li>
               {" "}
               <Link href='/menu'>
-                <a className=' underline decoration-sky-500 '>Menu</a>
+                <a className='no-underline hover:underline '>
+                  <h3>Menu</h3>
+                </a>
               </Link>
             </li>
           </ul>
         </div>
-        <div className='underline decoration-sky-500'>
-          <h1>Order Here!</h1>
+        <div className='no-underline hover:underline decoration-sky-500'>
+          <h3>Order Here!</h3>
         </div>
       </nav>
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      display: ["Pally", "Comic Sans MS", "sans-serif"],
+      body: ["Pally", "Comic Sans MS", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      black: colors.black,
+      primary: "#fff89a",
+      secondary: "#ffc900",
+      accent: "#086e7d",
+      accent2: "#1a5f7a",
+    },
     extend: {},
   },
   plugins: [
